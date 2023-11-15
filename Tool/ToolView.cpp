b0c886f1d787619cf8afcb54e11e772463b64763
messier3331 <148570516+messier3331@users.noreply.github.com>
2023-11-15 20:44:06 +0900

    15일 밤

--- Tool/ToolView.cpp
+++ Tool/ToolView.cpp
@@ -283,7 +283,7 @@ void CToolView::OnDestroy()
 
 	CTextureMgr::Destroy_Instance();
 	CDevice::Destroy_Instance();
-
+	
 }
 
 
@@ -293,7 +293,7 @@ void CToolView::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
 	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
 
 
-
+/// 여기서 구조 조정
 
 
 	CScrollView::OnKeyDown(nChar, nRepCnt, nFlags);
